de7ad584f3b372095c81473b9513507d33fe42ed
James Demlow <jdemlow@gmail.com>
2023-02-20 14:03:40 -0700

    add busStops JS file to flashcards folder

--- Flashcards/busStop.js
+++ Flashcards/busStop.js
@@ -1,5 +1,11 @@
-var number = function (busStops) {
-  // Good Luck!
-};
-
 //https://www.codewars.com/kata/5648b12ce68d9daa6b000099/javascript
+
+const number = function (busStops) {};
+
+console.log(
+  number([
+    [10, 0],
+    [3, 5],
+    [5, 8],
+  ])
+);
