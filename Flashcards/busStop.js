521d4a8887c5bbd554f31831aab3eaef487a2f63
James Demlow <jdemlow@gmail.com>
2023-02-21 15:22:07 -0700

    add distanceToPump JS file

--- Flashcards/busStop.js
+++ Flashcards/busStop.js
@@ -1,6 +1,11 @@
-//https://www.codewars.com/kata/5648b12ce68d9daa6b000099/javascript
-
-const number = function (busStops) {};
+const number = function (busStops) {
+  let totalPeople = 0;
+  for (i = 0; i < busStops.length; i++) {
+    totalPeople += busStops[i][0];
+    totalPeople -= busStops[i][1];
+  }
+  return totalPeople;
+};
 
 console.log(
   number([
@@ -9,3 +14,5 @@ console.log(
     [5, 8],
   ])
 );
+
+//https://www.codewars.com/kata/5648b12ce68d9daa6b000099/javascript
---
+++ Flashcards/distanceToPump.js
@@ -0,0 +1,3 @@
+console.log();
+
+//https://www.codewars.com/kata/5861d28f124b35723e00005e/solutions/javascript
