41a003b1b0588acfd01d336d6c645c367f88b90a
James Demlow <jdemlow@gmail.com>
2023-02-23 16:23:58 -0700

    add forLoop JS file to flashcards

---
+++ Flashcards/forLoop.js
@@ -0,0 +1,10 @@
+function pickIt(arr) {
+  let odd = [],
+    even = [];
+
+  return [odd, even];
+}
+
+console.log(pickIt([8, 1, 5, 4, 6, 1, 1]));
+
+//https://www.codewars.com/kata/5721a78c283129e416000999/solutions/javascript
