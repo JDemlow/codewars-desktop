e067a4a9ec6eef43e0aaf1f2d5929f83cce07459
James Demlow <jdemlow@gmail.com>
2023-03-04 19:32:58 -0700

    add mangoes JS file to flashcards

--- 8kyu-desktop/mangoes.js
+++ 8kyu-desktop/mangoes.js
@@ -1,8 +1,13 @@
+// function mango(quantity, price) {
+//   quantity = Math.trunc(quantity / 3) * 2 + (quantity % 3);
+//   return quantity * price;
+// }
+
 function mango(quantity, price) {
-  quantity = Math.trunc(quantity / 3) * 2 + (quantity % 3);
-  return quantity * price;
+  let freeMangos = Math.floor(quantity / 3);
+  return (quantity - freeMangos) * price;
 }
 
-console.log(mangoes(5, 3));
+console.log(mango(5, 3));
 
 //https://www.codewars.com/kata/57a77726bb9944d000000b06/solutions/javascript
---
+++ Flashcards/mangoes.js
@@ -0,0 +1,5 @@
+function mango(quantity, price) {}
+
+console.log(mango(5, 3));
+
+//https://www.codewars.com/kata/57a77726bb9944d000000b06/solutions/javascript
