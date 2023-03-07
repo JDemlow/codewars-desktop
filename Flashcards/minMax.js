efd13e682174288bbc868570e716e83c05eb9749
James Demlow <jdemlow@gmail.com>
2023-03-07 14:13:43 -0700

    add minMax to flashcards

---
+++ Flashcards/minMax.js
@@ -0,0 +1,6 @@
+let min = function (list) {};
+
+console.log(min([5, 9, 2, 5, 1, 3, 4]));
+console.log(max([5, 9, 2, 5, 1, 13, 3, 4]));
+
+//https://www.codewars.com/kata/577a98a6ae28071780000989/solutions/javascript
