18b895f1675274a980abf46e3ed2e88ac8e796f0
James Demlow <jdemlow@gmail.com>
2023-03-11 18:44:28 -0700

    add numberMethods JS file to flashcards

--- Flashcards/multiplyByEightOrNine.js
+++ Flashcards/multiplyByEightOrNine.js
@@ -1,6 +1,7 @@
-function simpleMultiplication(number) {}
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
 
 console.log(simpleMultiplication(9));
-console.log(122);
 
 //https://www.codewars.com/kata/583710ccaa6717322c000105/solutions/javascript
---
+++ Flashcards/numberMethods.js
@@ -0,0 +1,6 @@
+function colorOf(r, g, b) {}
+console.log(colorOf(1, 2, 3));
+
+console.log(colorOf(0, 111, 0));
+
+//https://www.codewars.com/kata/57238ceaef9008adc7000603/solutions/javascript
