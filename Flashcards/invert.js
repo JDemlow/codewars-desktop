8e3b197a0c2a36e851454595730d598868a3ff55
James Demlow <jdemlow@gmail.com>
2023-04-05 21:27:34 -0600

    modify invert.js file

--- Flashcards/invert.js
+++ Flashcards/invert.js
@@ -1,13 +1,9 @@
 function invert(array) {
-  let inverted = [];
-  array.map((num) => {
-    inverted.push(-num);
-  });
-  return inverted;
+  
 }
 
 console.log(invert([1, -2, 3, -4, 5]));
 
-console.log(321);
+console.log(123);
 
 //https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/solutions
