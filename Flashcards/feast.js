d1e988e84c982b4c40e8b70a0293dafb3aa9fb1d
James Demlow <jdemlow@gmail.com>
2023-05-09 23:17:31 -0600

    add files to flashcards folder

--- Flashcards/feast.js
+++ Flashcards/feast.js
@@ -3,3 +3,5 @@ function feast(beast, dish) {}
 console.log(feast("care bear", "car star"));
 
 //https://www.codewars.com/kata/5aa736a455f906981800360d/solutions/javascript
+
+//https://www.codewars.com/kata/5aa736a455f906981800360d/solutions/javascript
