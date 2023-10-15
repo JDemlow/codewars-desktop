4bf5f5b1b9fedd6257ae48e632666edcfaa0c91c
JDemlow <jdemlowdev@gmail.com>
2023-10-14 21:44:41 -0600

    modified flashcards

--- Flashcards/isUppercase.js
+++ Flashcards/isUppercase.js
@@ -11,6 +11,6 @@
 
 String.prototype.isUpperCase = function () {};
 
-console.log();
+console.log("hello I AM DONALD".isUpperCase());
 
 //https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/solutions
